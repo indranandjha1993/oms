944b4eb4b55c10dfc67e5d9e1f77ad4e8526ddb1
Valet2You <indra.jha@viralops.com>
2022-03-12 13:57:34 +0530

    admin text chnaged

--- app/order_management/urls.py
+++ app/order_management/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = "OMS Admin"
+admin.site.site_title = "OMS Admin Portal"
+admin.site.index_title = "Welcome to OMS Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
